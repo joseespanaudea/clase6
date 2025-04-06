6afa34af766788f4b82a106318b38cbe1dcb0ca6
joseespanaudea <jose.espana@udea.edu.co>
2025-04-06 16:38:49 -0500

    Ingreso de fechas

--- SistVMultMed.py
+++ SistVMultMed.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Medicamento:
     def __init__(self):
         self.__nombre = "" 
@@ -117,7 +119,13 @@ def main():
                 nombre=input("Ingrese el nombre de la mascota: ")
                 tipo=input("Ingrese el tipo de mascota (felino o canino): ").lower()
                 peso=int(input("Ingrese el peso de la mascota: "))
-                fecha=input("Ingrese la fecha de ingreso (dia/mes/año): ")
+                while True:
+                    fecha=input("""Ingrese la fecha de ingreso (dia/mes/año): """)
+                    try:
+                        datetime.strptime(fecha, "%d/%m/%Y")
+                        break
+                    except ValueError:
+                        print("""Ocurrio un error, la fecha debe tener formato "dd/mm/aaaa". Ingresela nuevamente""")
                 nm=int(input("Ingrese cantidad de medicamentos: "))
                 lista_med=[]
 
